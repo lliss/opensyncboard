d22d4a69c122dabaf763ab810776175c3aadb8ae
lliss <lliss@users.noreply.github.com>
2020-05-22 12:29:15 -0400

    use event history to redraw. move padding into javascript to handle calculating height and width.

--- public/javascript/draw.js
+++ public/javascript/draw.js
@@ -6,16 +6,18 @@ let lineWidth = 4;
 let lineColor = '#000000';
 let events = [];
 let lastImage = null;
+const paddingPercent = 5;
 
 function main() {
   canvas = document.querySelector('#syncboard');
+  document.body.style.padding = `${paddingPercent}vh ${paddingPercent}vw`;
   ctx = canvas.getContext('2d');
   ctx.lineWidth = lineWidth;
   resetCanvasSize();
 
   window.addEventListener('resize', (evnt) => {
     resetCanvasSize();
-    // redraw();
+    redraw();
   });
 
   canvas.addEventListener('mousedown', (evnt) => {
@@ -29,47 +31,71 @@ function main() {
 
   canvas.addEventListener('mousemove', (evnt) => {
     if (drawing) {
+      console.log(evnt);
       let position = getCanvasPositionByEvent(evnt);
+
       ctx.beginPath();
       ctx.moveTo(lastPosition.x, lastPosition.y);
       ctx.lineTo(position.x, position.y);
       ctx.stroke();
-      events.push({from: lastPosition, to: position, width: lineWidth, color: lineColor});
+      let lastPositionRelative = makeRelativePosition(lastPosition);
+      let currentPositionRelative = makeRelativePosition(position);
+
+      events.push({
+        from: lastPositionRelative,
+        to: currentPositionRelative,
+        width: lineWidth / canvas.width,
+        color: lineColor
+      });
       lastPosition = position;
     }
   });
 
 }
 
+function makeRelativePosition(position) {
+  return {
+    x: position.x / canvas.width,
+    y: position.y / canvas.height,
+  };
+}
+
+function makeAbsolutePosition(position) {
+  return {
+    x: Math.floor(position.x * canvas.width),
+    y: Math.floor(position.y * canvas.height),
+  };
+}
+
 function getCanvasPositionByEvent(evnt) {
   return {
-    x: evnt.clientX - evnt.target.offsetLeft,
-    y: evnt.clientY - evnt.target.offsetTop
+    x: evnt.pageX - evnt.target.offsetLeft,
+    y: evnt.pageY - evnt.target.offsetTop
   };
 }
 
 function resetCanvasSize() {
-  let width = Math.floor(window.innerWidth * .9);
-  if (events.length > 0 || lastImage === null) {
-    lastImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    events = [];
-  }
+  let width = Math.floor(window.innerWidth * (1 - paddingPercent * 2 / 100));
   canvas.width = width;
   canvas.height = Math.floor(width * 9 / 16);
-  let imagePromise = createImageBitmap(lastImage);
-  imagePromise.then((image) => {
-    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-  });
+
+  if (window.innerHeight < document.body.scrollHeight) {
+    console.log('trigger');
+    let height = Math.floor(window.innerHeight * (1 - paddingPercent * 2 / 100));
+    canvas.height = height;
+    canvas.width = Math.floor(height * 16 / 9);
+  }
 }
 
-// function redraw() {
-
-//   events.forEach((evnt) => {
-//     ctx.beginPath();
-//     ctx.moveTo(evnt.from.x, evnt.from.y);
-//     ctx.lineTo(evnt.to.x, evnt.to.y);
-//     ctx.stroke();
-//   });
-// }
+function redraw() {
+  events.forEach((evnt) => {
+    let fromPosition = makeAbsolutePosition(evnt.from);
+    let toPosition = makeAbsolutePosition(evnt.to);
+    ctx.beginPath();
+    ctx.moveTo(fromPosition.x, fromPosition.y);
+    ctx.lineTo(toPosition.x, toPosition.y);
+    ctx.stroke();
+  });
+}
 
 main();
